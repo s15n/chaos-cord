0dd905af65a7f41a834aa3314b75d60125c8e005
FauxKiwi <sfn14ok04@gmail.com>
2021-10-12 20:17:49 +0200

    Added Rest Proxy(JS) concept

--- src/discord/rest/Rest.ts
+++ src/discord/rest/Rest.ts
@@ -1,11 +1,38 @@
 import { DiscordClient } from "../DiscordClient";
 
-class Route {
-    _: string
-    constructor(path: string) {
-        this._ = path
+const noop = () => {}; // eslint-disable-line no-empty-function
+const methods = ['get', 'post', 'delete', 'patch', 'put']
+const reflectors = [
+  'toString',
+  'valueOf',
+  'inspect',
+  'constructor',
+  Symbol.toPrimitive,
+  Symbol.for('nodejs.util.inspect.custom')
+];
+
+export function Rest(api: string) {
+    const route = [api]
+    const handler: ProxyHandler<() => void> = {
+        get(_, path) {
+            if (reflectors.includes(path)) return () => route.join('/')
+
+            if (typeof path !== 'string') return
+
+            if (methods.includes(path)) {
+                return (props?: RequestInit) => {
+                    return fetch("", {
+                        method: path,
+                        ...props
+                    })
+                }
+            }
+
+            route.push(path)
+            return new Proxy(noop, handler)
+        },
     }
-    
+    return new Proxy(noop, handler) as any
 }
 
 export const API = null
